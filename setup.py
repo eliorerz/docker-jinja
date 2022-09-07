5b3751311574533c58e8d829abe237b8a8d65494
Elior Erez <eerez@redhat.com>
2022-09-07 15:21:18 +0300

    Add support on python3.10

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ settings = dict()
 long_description = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 
 settings.update(
-    name='docker-jinja',
+    name='docker-jinja3',
     version='1.0.0',
     description='Extend your dockerfiles with Jinja2 syntax to to more awesome dockerfiles',
     long_description=long_description,
@@ -18,25 +18,14 @@ settings.update(
     packages=find_packages(exclude=['.tox', '*test/']),
     scripts=['scripts/dj'],
     install_requires=[
-        'PyYAML==3.11',
-        'Jinja2==2.7.3',
+        'PyYAML==6.0',
+        'Jinja2==3.1.2',
         'docopt==0.6.2',
     ],
     license="MIT",
     url='https://github.com/Grokzen/docker-jinja',
     classifiers=(
-        #  As from https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        # 'Development Status :: 1 - Planning',
-        # 'Development Status :: 2 - Pre-Alpha',
-        'Development Status :: 3 - Alpha',
-        # 'Development Status :: 4 - Beta',
-        # 'Development Status :: 5 - Production/Stable',
-        # 'Development Status :: 6 - Mature',
-        # 'Development Status :: 7 - Inactive',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.10',
         'Environment :: Console',
         'Operating System :: POSIX',
     )
